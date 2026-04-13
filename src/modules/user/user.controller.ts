cd764dfd324dcd6aa95b0d0d92d49b89a56a8c76
Pramod Ghimire <pramodghimire96@gmail.com>
2026-04-13 15:23:12 +0545

    feat: add is public decorator

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,22 +1,24 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from '../../bll/user.service';
 import { JwtAuthGuard } from 'src/strategies/guards/jwt.auth.guard';
 import { RegisterDto } from 'src/dto/user/register.dto';
 import { ResponseHandlerService } from 'src/common/response/response-handler.service';
-import { ApiExtraModels, ApiResponse } from '@nestjs/swagger';
+import { ApiExtraModels } from '@nestjs/swagger';
 import { ReadUserDto } from 'src/dto/user/read-user.dto';
 import { GenericResponseDto } from 'src/dto/generic-response.dto';
 import { ApiGenericResponse } from 'src/decorator/generic-response.decorator';
+import { Public } from 'src/decorator/is-public.decorator';
 
 @Controller('users')
 @ApiExtraModels(ReadUserDto, RegisterDto, GenericResponseDto)
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(
     private readonly userService: UserService,
     private readonly responseHandler: ResponseHandlerService,
   ) {}
 
+  @Public()
   @Post('register')
   register(@Body() dto: RegisterDto) {
     return this.userService.register(dto);
